f4476ecddebc6c86ad5c06b006d778ae883e3d12
miyazakikakeru <>
2022-10-28 14:39:26 +0900

    first commit

---
+++ src/app/Mail/ContactThanks.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactThanks extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $inputs;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($inputs)
+    {
+        $this->inputs = $inputs;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to($this->inputs['mail'])->markdown('emails.contact.thanks', $this->inputs);
+    }
+}
