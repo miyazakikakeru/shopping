2728529b532ac129cdd9f5110c65510f9c85fb39
miyazakikakeru <>
2022-10-31 16:08:33 +0900

    2022_10_31_070555_create_admin_table

--- src/database/migrations/2022_10_31_070716_create_product_table.php
+++ src/database/migrations/2022_10_31_070716_create_product_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->id();
-
             $table->string('name');
-            $table->string('password');
-
+            $table->text('explanation');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product');
     }
 };
