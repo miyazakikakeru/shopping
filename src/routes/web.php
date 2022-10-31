42cfccc8a8ba8b4dde067204fb2d0056e6b86617
business14 <>
2022-10-31 15:36:10 +0900

    adasd

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,12 +20,8 @@ Route::post('/', [ConrtactController::class, 'check']);
 Route::post('/home', [ConrtactController::class, 'home']); 
 Route::post('/home/search', [ConrtactController::class, 'search']);
 Route::post('/home/search/detail', [ConrtactController::class, 'detail']);//詳細
-<<<<<<< HEAD
 
-//admin
 Route::get('/admin', [ConrtactController::class, 'adminlogin']);
 Route::post('/admin', [ConrtactController::class, 'admincheck']);
 Route::post('/admin/home', [ConrtactController::class, 'adminhome']); 
 
-=======
->>>>>>> 967afdeb9b3dac6ef0cc2f3f33e5a4b45374e8fa
