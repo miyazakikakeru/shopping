f3b5eaf46a53ae9a2c3130eedc2e672477fd4905
business14 <>
2022-10-31 16:22:55 +0900

    bfgdf

--- src/app/Http/Controllers/ConrtactController.php
+++ src/app/Http/Controllers/ConrtactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\ConrtactRequest;
 use App\Http\Controllers\component;
 use App\Mail\ContactThanks;
