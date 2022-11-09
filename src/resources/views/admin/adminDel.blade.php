166492f42adf9485022c7a381b1f92b695332df7
business14 <>
2022-11-09 14:59:22 +0900

    dsa

---
+++ src/resources/views/admin/adminDel.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.adminLayouts.layout')
+@section('5','active')
+@section('content')
+<h2>管理者削除</h2>
+@endsection
---
+++ src/resources/views/admin/adminIn.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.adminLayouts.layout')
+@section('4','active')
+@section('content')
+<h2>管理者登録</h2>
+@endsection
