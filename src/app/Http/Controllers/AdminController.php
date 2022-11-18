381c57c84600b1a70db09e7911dd090d7a3fff87
business14 <>
2022-11-18 15:39:14 +0900

    FCJKHLJKPL

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Admin;
 use App\Models\Product;
-use App\Http\Requests\admin\AdminRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\component;
@@ -16,8 +15,7 @@ class AdminController extends Controller
         if(count(Admin::where('id',$request->session()->get('id'))->where('password',$request->session()->get('password'))->get())>0){
             return redirect('/admin/home');
         }
-        $inputs = $request->all();
-        return view('admin.adminlogin',$inputs);
+        return view('admin.adminlogin');
     }
     public function check(Request $request){
         $request->session()->put('id',$request->id);
