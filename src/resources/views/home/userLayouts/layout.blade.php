3adb54639274420d73b95a282c0e7feeee3c5bec
business14 <>
2022-12-08 16:31:59 +0900

    hgbnjmkl;,:.

--- src/resources/views/home/userLayouts/layout.blade.php
+++ src/resources/views/home/userLayouts/layout.blade.php
@@ -11,17 +11,13 @@
     <!-- Styles -->
 </head>
 <body>
-<nobr>
-<h1 style="text-align:right;">
-<div style="padding:15px 80px">
+<header style="padding:15px 100px;">
     {{App\Models\User::where('mail_address',Session::get('mail_address'))->first()->value('name')}}さん    
     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" >
     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
     </svg>
-</div>
-</h1>
+</header>
 
-</nobr>
 @yield('content')
 </body>
